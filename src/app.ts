cc6322fab2e6caa921498724fec193475e4ea2a8
Ahmad Sorour <ahmad_sorour@outlook.com>
2022-12-24 10:53:13 +0200

    added dotenv config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 import authRouter from '@routes/auth';
 import handleError from '@middlewares/errorHandler';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
