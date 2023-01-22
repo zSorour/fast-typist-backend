a569c7e5207fd7ba367efbd2d0357d30c467e547
Ahmad Sorour <ahmad_sorour@outlook.com>
2023-01-22 13:17:54 +0200

    fix: now binding the authController as the this keyword in the called controller functions

--- src/features/auth/Auth.router.ts
+++ src/features/auth/Auth.router.ts
@@ -10,17 +10,20 @@ const authController = new AuthController();
 authRouter.post(
   '/login',
   validate(loginSchema, 'Unable to log you in'),
-  authController.login
+  authController.login.bind(authController)
 );
 authRouter.post(
   '/register',
   validate(registerSchema, 'Unable to create an account'),
-  authController.register
+  authController.register.bind(authController)
 );
-authRouter.post('/logout', authController.logout);
+authRouter.post('/logout', authController.logout.bind(authController));
 
-authRouter.post('/refresh', authController.refresh);
+authRouter.post('/refresh', authController.refresh.bind(authController));
 
-authRouter.post('/verify-logged-in', authController.dummyVerifyLoggedIn);
+authRouter.post(
+  '/verify-logged-in',
+  authController.dummyVerifyLoggedIn.bind(authController)
+);
 
 export default authRouter;
