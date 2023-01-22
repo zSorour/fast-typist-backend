07fb840f5ee04cad06d458ad4a1fe2e34c08de0b
Ahmad Sorour <ahmad_sorour@outlook.com>
2023-01-22 13:19:54 +0200

    chore: origin is now specified to be the current frontend origin

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import initSocketServer from '@config/io';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
 app.use(cookieParser());
 app.use(express.json());
 
